b143aec6ba3fbddaddb6126d9b38879a5866f9d8
Jack Cromptonm <jackcrompton8@gmail.com>
2018-01-14 20:22:03 +0000

    trying to fix everything

--- src/App.vue
+++ src/App.vue
@@ -21,60 +21,12 @@ export default {
     }
 </script>
 <style>
-    /**
-    * 1. Avoid the IE 10-11 `min-height` bug.
-    * 2. Set `flex-shrink` to `0` to prevent some browsers from
-    *    letting these items shrink to smaller than their content's default
-    *    minimum size. See http://bit.ly/1Mn35US for details.
-    * 3. Use `%` instead of `vh` since `vh` is buggy in older mobile Safari.
-    */
-
-    .Site {
+    #app {
         display: flex;
         flex-direction: column;
-        height: 100%;
-        /* 1, 3 */
+        justify-content: center;
+        flex: 1;
     }
-
-    .Site-header,
-    .Site-footer {
-        flex: none;
-        /* 2 */
-    }
-
-    .Site-content {
-        flex: 1 0 auto;
-        /* 2 */
-        padding: var(--space) var(--space) 0;
-        width: 100%;
-    }
-
-    .Site-content::after {
-        content: '\00a0';
-        /* &nbsp; */
-        display: block;
-        margin-top: var(--space);
-        height: 0px;
-        visibility: hidden;
-    }
-
-    @media (--break-lg) {
-        .Site-content {
-            padding-top: var(--space-lg);
-        }
-        .Site-content::after {
-            margin-top: var(--space-lg);
-        }
-    }
-
-    .Site-content--full {
-        padding: 0;
-    }
-
-    .Site-content--full::after {
-        content: none;
-    }
-
     body,
     html {
         width: 100%;
@@ -85,49 +37,23 @@ export default {
         text-align: center;
         color: #2c3e50;
         margin: 0px;
+        display: flex;
         background-color: #ededed;
-        text-rendering: optimizeLegibility;
     }
-
     h1,
     h2 {
         font-weight: normal;
     }
-
     ul {
         list-style-type: none;
         padding: 0;
     }
-
     li {
         display: inline-block;
         margin: 0 10px;
     }
-
     a {
         color: #42b983;
     }
-
-    :root {
-        --space: 1.5em;
-        --space-lg: 2em;
-        --bg-color: hsl(150, 45%, 50%);
-    }
-
-    *,
-    *::after,
-    *::before {
-        box-sizing: border-box;
-    }
-
-    /* @custom-media --break-sm (min-width: 384px);
-    @custom-media --break-md (min-width: 576px);
-    @custom-media --break-lg (min-width: 768px);
-    @custom-media --high-dppx (-webkit-min-device-pixel-ratio: 1.5),
-    (min-resolution: 144dpi),
-    (min-resolution: 1.5dppx); 
-    npm postcss
-    */
-    
 </style>
 <style scoped></style>
--- src/eventbus.js
+++
@@ -1,4 +0,0 @@
-/*jshint esversion: 6 */
-import Vue from 'vue';
-const EventBus = new Vue();
-export default EventBus;
